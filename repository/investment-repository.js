72523791aaa1bfec3a6f84f8b403916abe5723a1
Paulo Alves <contato@pauloalves.me>
2021-08-23 17:27:16 -0300

    feat: export function to get report by investment id

--- repository/investment-repository.js
+++ repository/investment-repository.js
@@ -85,5 +85,6 @@ export default {
     getInvestmentsById,
     getInvestmentsByName,
     getReportsById,
-    getReportsByInvestmentName
+    getReportsByInvestmentName,
+    getReportsByInvestmentId
 }

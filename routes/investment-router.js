21daa813695ef75608b962ca024fd51b58e6a3d9
Paulo Alves <contato@pauloalves.me>
2021-08-23 17:53:43 -0300

    feat: add all the routes

---
+++ routes/investment-router.js
@@ -0,0 +1,18 @@
+import express from 'express';
+const router = express.Router();
+import InvestmentController from '../controllers/investment-controller.js';
+
+router.get('/', InvestmentController.getInvestments);
+router.get('/reports', InvestmentController.getReports);
+router.get('/id/:id', InvestmentController.getInvestmentsById);
+router.get('/name/:name', InvestmentController.getInvestmentByName);
+router.get('/reports/id/:id', InvestmentController.getReportById);
+router.get('/reports/name/:name', InvestmentController.getReportsbyInvestmentName);
+router.get('/reports/invesmentid/:id', InvestmentController.getReportsbyInvestmentId)
+router.get('/reports/month/:month', InvestmentController.getReportsByMonth);
+router.get('/reports/year/:year', InvestmentController.getReportsByYear);
+router.get('/reports/date/:month-:year', InvestmentController.getReportsByDate);
+router.get('/reports/orderbyvalue/asc', InvestmentController.getReportsByValueAsc);
+router.get('/reports/orderbyvalue/desc', InvestmentController.getReportsByValueDesc);
+
+export default router;
